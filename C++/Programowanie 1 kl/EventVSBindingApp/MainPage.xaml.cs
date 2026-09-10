2219b95eefc83410aff5a1df1510fd5254084dcb
Uczen <Uczen@LAPTOP-26CG11IL>
2026-09-10 14:59:59 +0200

    kotki jygyjfhvaghvgfsghfghfghsfhghfgsfvhsvg cos wsumie nie pamietam a sory binding of isaaka morela

--- C++/Programowanie 1 kl/EventVSBindingApp/MainPage.xaml.cs
+++ C++/Programowanie 1 kl/EventVSBindingApp/MainPage.xaml.cs
@@ -14,5 +14,33 @@
                 && sender is Slider slider)
                 RotationLabel.Rotation = slider.Value;
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            pokaszLabel.Text = podajEntry.Text;
+            podajEntry.Text = "";
+
+        }
+
+        public string Message { get; set; }
+
+        private string returnMessage;
+        public string ReturnMessage
+        {
+
+            get { return returnMessage; }
+
+            set
+            {
+                returnMessage = value;
+                //OnPropertyChanged(nameof("ReturnMessage");
+                OnPropertyChanged();
+            }
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            ReturnMessage = Message;
+        }
     }
 }
